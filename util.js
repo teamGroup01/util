23ea070d4c59577995001b94f06cea0d05b873e0
htmlcssscript <htmlcssscript@users.noreply.github.com>
2017-11-18 20:46:25 +0800

    Update util.js

--- util.js
+++ util.js
@@ -139,3 +139,23 @@ function dataScope(value1, value2) {
     }
     return dateArr;
 }
+
+/*判断浏览器版本*/
+ export function version () {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isOpera = userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera; //判断是否IE浏览器
+    var isEdge = userAgent.indexOf("Windows NT 6.1; Trident/7.0;") > -1 && !isIE; //判断是否IE的Edge浏览器
+    var isFF = userAgent.indexOf("Firefox") > -1; //判断是否Firefox浏览器
+    var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1; //判断是否Safari浏览器
+    var isChrome = userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1; //判断Chrome浏览器
+
+    if (isIE) {
+      var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+      reIE.test(userAgent);
+      var fIEVersion = parseFloat(RegExp["$1"]);
+      if(fIEVersion < 9) {
+        alert('当前浏览器版本过低');
+      }
+    }
+  }
